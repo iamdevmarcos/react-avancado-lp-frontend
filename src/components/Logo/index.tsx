944f0c30f76673fd4ae82960658ca31137c0d567
Marcos Andre <marcosdev.me@gmail.com>
2022-07-03 07:16:34 -0300

    feat: add util to get image url

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
-import { LogoProps } from 'types/api'
 import * as S from './styles'
 
+import { LogoProps } from 'types/api'
+import { getImageUrl } from 'utils/getImageUrl'
+
 const Logo = ({ alternativeText, url }: LogoProps) => (
-  <S.LogoWrapper src={`http://localhost:1337${url}`} alt={alternativeText} />
+  <S.LogoWrapper src={getImageUrl(url)} alt={alternativeText} />
 )
 
 export default Logo
